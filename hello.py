27d214a0db8a410cd06e3f4241943e62d34e1d3b
zzz945 <642010820@qq.com>
2016-11-01 16:07:57 +0800

    test

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
 import tushare as ts
 
-df = ts.get_stock_basics()
 #data = ts.get_h_data('399001', index=True) #深证成指
 #data = ts.get_h_data('399006', index=True) #创业板指
-data = ts.get_h_data('000001', index=True) #沪指
-print(data)
+#data = ts.get_h_data('000001', index=True) #沪指
+""""
+df1 = ts.get_growth_data(2012, 4)
+df2 = ts.get_growth_data(2013, 4)
+df3 = ts.get_growth_data(2014, 4)
+df4 = ts.get_growth_data(2015, 4)
+df5 = ts.get_growth_data(2016, 3)
+print(df1)
+"""
+import random
+print(random.randint(0, 99))
+
